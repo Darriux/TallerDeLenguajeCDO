ad1a2fb9832210d1c5f6e8f82b65d9f4c1bfc3a4
Darriux <daribosate7@gmail.com>
2025-03-24 23:34:17 -0300

    Evitar el uso de indices

--- Entregable/questions.py
+++ Entregable/questions.py
@@ -1,7 +1,6 @@
 import random
 import sys
 
-puntaje = 0
 # Preguntas para el juego
 questions = [
     "¿Qué función se usa para obtener la longitud de una cadena en Python?",
@@ -27,12 +26,12 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-# Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+puntaje = 0
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+for question, answer_options, correct_index in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(question)
+    for i, answer in enumerate(answer_options):
         print(f"{i + 1}. {answer}")
 
     # El usuario tiene 2 intentos para responder correctamente
@@ -40,7 +39,7 @@ for _ in range(3):
         user_answer = int(input("Respuesta: ")) - 1
         # Se verifica si la respuesta es correcta
         if user_answer in correct_answers_index:
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == correct_index:
                 print("¡Correcto!")
                 puntaje += 1
                 break
@@ -48,7 +47,7 @@ for _ in range(3):
                 # Si el usuario no responde correctamente después de 2 intentos,
                 # se muestra la respuesta correcta
                 print("Incorrecto. La respuesta correcta es:")
-                print(answers[question_index][correct_answers_index[question_index]])
+                print(answer_options[correct_index])
                 puntaje -= 0.5
         else:
             print(f"Respuesta no válida")
